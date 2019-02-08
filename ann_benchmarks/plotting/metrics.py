Fix the scaling of knn

ann_benchmarks/plotting/metrics.py
@@ -21,7 +21,7 @@ def knn(dataset_distances, run_distances, count, metrics, epsilon=1e-10):
         knn_metrics.attrs['mean'] = np.mean(recalls) / float(count)
         knn_metrics.attrs['std'] = np.std(recalls) / float(count)
         percentiles = [5,25,50,75,95]
-        percentile_values = np.percentile(recalls, percentiles)
+        percentile_values = np.percentile(recalls/float(count), percentiles)
         for p, v in zip(percentiles, percentile_values):
             knn_metrics.attrs['perc-' + str(p)] = v
         knn_metrics['recalls'] = recalls
@@ -49,7 +49,7 @@ def epsilon(dataset_distances, run_distances, count, metrics, epsilon=0.01):
         epsilon_metrics.attrs['mean'] = np.mean(recalls) / float(count)
         epsilon_metrics.attrs['std'] = np.std(recalls) / float(count)
         percentiles = [5,25,50,75,95]
-        percentile_values = np.percentile(recalls, percentiles)
+        percentile_values = np.percentile(recalls/float(count), percentiles)
         for p, v in zip(percentiles, percentile_values):
             epsilon_metrics.attrs['perc-' + str(p)] = v
         epsilon_metrics['recalls'] = recalls
@@ -80,9 +80,9 @@ def queries_per_second(query_times, metrics):
         qps_metrics = metrics.create_group('qps')
         qps_metrics.attrs['mean'] = 1/np.mean(query_times)
         percentiles = [5,25,50,75,95]
-        percentile_values = np.percentile(query_times, percentiles)
+        percentile_values = np.percentile(1/query_times, percentiles)
         for p, v in zip(percentiles, percentile_values):
-            qps_metrics.attrs['perc-' + str(p)] = 1/v
+            qps_metrics.attrs['perc-' + str(p)] = v
         qps_metrics['query_times'] = query_times
     else:
         print("Found cached result")